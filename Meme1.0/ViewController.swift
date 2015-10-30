33d5975fe41c423fe199259967c5b8ee7b555ba5
Pratyush Varchaswee <pratyushvarchaswee@Pratyushs-MBP.home>
2015-10-29 23:27:42 -0400

    Initial Commit

---
+++ Meme1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme1.0
+//
+//  Created by Pratyush Varchaswee on 10/29/15.
+//  Copyright (c) 2015 Pratyush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
