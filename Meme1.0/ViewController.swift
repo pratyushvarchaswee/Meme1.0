77a6873141505d0913427d9217b5bfed36286e80
pratyushvarchaswee <pratyushvarchaswee@Pratyushs-MBP.home>
2015-11-01 12:37:42 -0500

    MemeApp 1.0
    This will be initial version of app , It has currently only initial
    files .

--- Meme1.0/ViewController.swift
+++ Meme1.0/ViewController.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate  {
 
+   
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pickAnImage(sender: AnyObject) {
+        let imagePicker=UIImagePickerController()
+        imagePicker.delegate=self
+        self.presentViewController(imagePicker, animated:true, completion: nil)
+//        self.dismissViewControllerAnimated(true,completion: nil)
+        
     }
-
+    
 
 }
 
